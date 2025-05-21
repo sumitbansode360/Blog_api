2c765535b13954fa080777024b7e9f14f6a94861
sumitbansode360 <sumitbansode360@gmail.com>
2025-05-21 18:39:31 +0530

    models created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='blog_images')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    bio = models.CharField(max_length=400)
+    profile = models.ImageField(upload_to='user_images')
+
+    def __str__(self):
+        return self.username
