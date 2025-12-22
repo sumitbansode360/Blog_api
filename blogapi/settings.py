82ff826d95eca336c3750b4a5bb5f0ce816b96bd
sumitbansode360 <sumitbansode360@gmail.com>
2025-12-22 22:56:45 +0530

    added default file storage to settings

--- blogapi/settings.py
+++ blogapi/settings.py
@@ -133,6 +133,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
